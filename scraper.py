0ce88bb30a7a2656114005ec97c4762f920ca6f9
Mark Lavrentyev <lavrema@outlook.com>
2018-03-25 12:19:02 -0400

    Add 'decided_by_auto' field to matchdata

--- scraper.py
+++ scraper.py
@@ -8,32 +8,42 @@ from datetime import datetime, date
 
 tba_base_url = "https://www.thebluealliance.com/api/v3/"
 
+
 class MatchData:
     def __init__(self, event_key, match_key, week, red_score, red_auto, blue_score, blue_auto):
         self.event = event_key
         self.match = match_key
         self.week = week
 
-        self.red_score = red_score
-        self.red_auto = red_auto
-        self.blue_score = blue_score
-        self.blue_auto = blue_auto
+        self.red_score = int(red_score)
+        self.red_auto = int(red_auto)
+        self.blue_score = int(blue_score)
+        self.blue_auto = int(blue_auto)
 
-        if red_score > blue_score:
+        if self.red_score > self.blue_score:
             self.winner = "red"
-        elif blue_score > red_score:
+        elif self.blue_score > self.red_score:
             self.winner = "blue"
         else:
             self.winner = "tie"
 
-        if red_auto > blue_auto:
+        if self.red_auto > self.blue_auto:
             self.auto_winner = "red"
-        elif blue_auto > red_auto:
+        elif self.blue_auto > self.red_auto:
             self.auto_winner = "blue"
         else:
             self.auto_winner = "tie"
 
         self.winners_match = self.winner == self.auto_winner and self.winner != "tie" and self.auto_winner != "tie"
+
+        if self.red_score - self.red_auto > self.blue_score - self.blue_auto:
+            winner_no_auto = "red"
+        elif self.blue_score - self.blue_auto > self.red_score - self.red_auto:
+            winner_no_auto = "blue"
+        else:
+            winner_no_auto = "tie"
+
+        self.decided_by_auto = winner_no_auto != self.winner
 
 
 class Event:
@@ -57,9 +67,10 @@ def get_completed_events(year):
         event_end_date = datetime.strptime(event["end_date"], "%Y-%m-%d").date()
         event_start_date = datetime.strptime(event["start_date"], "%Y-%m-%d").date()
         if event_end_date < current_date and 0 <= event["event_type"] <= 6:
-            all_events.append(Event(event["key"], event["week"], event_start_date, event_end_date))
+            all_events.append(Event(event["key"], event["week"]+1, event_start_date, event_end_date))
 
     return all_events
+
 
 def get_all_matches(events):
     all_matches = []
@@ -80,8 +91,10 @@ def get_all_matches(events):
 
     return all_matches
 
+
 def pick_random_matches(matches, sample_size):
     return random.sample(matches, sample_size)
+
 
 def calc_sample_stats(matches):
     n = len(matches)
@@ -96,6 +109,7 @@ def calc_sample_stats(matches):
 
     return p, q, std_err, n
 
+
 def import_matches(path):
     if not os.path.exists(path):
         raise FileNotFoundError
@@ -109,6 +123,7 @@ def import_matches(path):
 
     return matches
 
+
 def export_matches(matches, path):
     if not os.path.exists(path.rsplit("/", 1)[0]):
         os.makedirs(path.rsplit("/", 1)[0])
@@ -117,19 +132,20 @@ def export_matches(matches, path):
         writer = csv.writer(file)
         writer.writerow(["event", "match", "week",
                          "red_score", "red_auto", "blue_score", "blue_auto",
-                         "winner", "auto_winner", "winners_match"])
+                         "winner", "auto_winner", "winners_match", "decided_by_auto"])
 
         for match in matches:
             row = vars(match).values()
             writer.writerow(row)
 
+
 if __name__ == "__main__":
-    # all_matches = import_matches("../data/all_matches.csv")
-    all_events = get_completed_events(2018)
-    all_matches = get_all_matches(all_events)
+    all_matches = import_matches("../data/all_matches.csv")
+    # all_events = get_completed_events(2018)
+    # all_matches = get_all_matches(all_events)
     export_matches(all_matches, "../data/all_matches.csv")
-    #
-    # selected_sample = pick_random_matches(all_matches, int(len(all_matches)*0.07))
+
+    # selected_sample = pick_random_matches(all_matches, int(len(all_matches)*0.09))
     # print(calc_sample_stats(selected_sample))
     #
     # export_matches(selected_sample, "../data/sample.csv")
